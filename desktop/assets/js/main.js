29c3e9b147afbc741f6c72fa7af688feda3dc171
ilham Rohmad Widayat <ilham.rohmad@kly.id>
2025-08-02 12:26:07 +0700

    update variable js

--- desktop/assets/js/main.js
+++ desktop/assets/js/main.js
@@ -26,48 +26,20 @@ document.querySelectorAll('.switch-theme').forEach((item)=>{
         console.log({checkMode}, value);
     })
 })
-// const header = document.querySelector('.header')
-// if (header) {
-//     // const
-//     //     navbarCollapsed = document.querySelector('.navbar--collapsed'),
-//     //     // headerMenu = header.querySelector('.header__menu'),
-//     //     navbarCollapsedClose = document.querySelector('.navbar--collapsed__top__close')
-//         // headerSearch = header.querySelector('.header--search__button'),
-//         // headerForm = header.querySelector('.header--search__form'),
-//         // headerFormClose = header.querySelector('.header--search__form__close'),
-
-//     // headerMenu.addEventListener('click',()=>{
-//     //     navbarCollapsed.classList.add('--active')
-//     // })
-//     // navbarCollapsedClose.addEventListener('click',()=>{
-//     //     navbarCollapsed.classList.remove('--active')
-//     // })
-//     // headerSearch.addEventListener('click',()=>{
-//     //     header.classList.add('--searchCollapsed')
-//     //     headerForm.classList.add('--active')
-//     // })
-//     // headerFormClose.addEventListener('click',()=>{
-//     //     header.classList.remove('--searchCollapsed')
-//     //     headerForm.classList.remove('--active')
-//     // })
-
-//     // document.querySelector('.submenu').addEventListener('click',(e)=>{
-//     //     e.target.querySelector('.submenu').classList.toggle('--active')
-//     // })
-// }
 
 let lastScrollTop = 0,
     lastHeaderPosition = 0
+const header = document.querySelector('.header')
 window.addEventListener('scroll', function() {
     //unsticky header
     let scrollHeaderTop = window.pageYOffset || document.documentElement.scrollTop,
-        limitHeader = document.querySelector('.header').offsetHeight;
+        limitHeader = header.offsetHeight;
     // if(window.innerWidth < 1024){
         if (scrollHeaderTop  > limitHeader){
             if (scrollHeaderTop  > lastHeaderPosition) {
-                document.querySelector('.header').classList.add('--unsticky')
+                header.classList.add('--unsticky')
             } else {
-                document.querySelector('.header').classList.remove('--unsticky')
+                header.classList.remove('--unsticky')
             }
         }
     // }
